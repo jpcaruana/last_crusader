f2d7f11a56545d9c70abf51ab19323e644aeab74
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2020-02-23 13:47:05 +0100

    first commit
    mix new . --sup --app last_crusader

---
+++ lib/last_crusader.ex
@@ -0,0 +1,18 @@
+defmodule LastCrusader do
+  @moduledoc """
+  Documentation for `LastCrusader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LastCrusader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/last_crusader_test.exs
@@ -0,0 +1,8 @@
+defmodule LastCrusaderTest do
+  use ExUnit.Case
+  doctest LastCrusader
+
+  test "greets the world" do
+    assert LastCrusader.hello() == :world
+  end
+end
