0ba663af84e75a985aadf9013b44cd2f1051d718
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2020-02-23 17:02:51 +0100

    refactor: extract functions into handler module

---
+++ lib/last_crusader/handler.ex
@@ -0,0 +1,28 @@
+defmodule LastCrusader.Handler do
+  @moduledoc """
+  Handles HTTP requests
+  """
+
+  def handle_events(conn) do
+    {status, body} =
+      case conn.body_params do
+        %{"events" => events} -> {200, process_events(events)}
+        _ -> {422, missing_events()}
+      end
+
+    Plug.Conn.send_resp(conn, status, body)
+  end
+
+  defp process_events(events) when is_list(events) do
+    # Do some processing on a list of events
+    Poison.encode!(%{response: "Received Events!"})
+  end
+  defp process_events(_) do
+    # If we can't process anything, let them know :)
+    Poison.encode!(%{response: "Please Send Some Events!"})
+  end
+
+  defp missing_events do
+    Poison.encode!(%{error: "Expected Payload: { 'events': [...] }"})
+  end
+end
--- lib/last_crusader/router.ex
+++ lib/last_crusader/router.ex
@@ -3,6 +3,7 @@ defmodule LastCrusader.Router do
   A Plug responsible for logging request info, parsing request body's as JSON,
   matching routes, and dispatching responses.
   """
+  alias LastCrusader.Handler, as: Handler
   use Plug.Router
 
   # This module is a Plug, that also implements it's own plug pipeline, below:
@@ -19,21 +20,6 @@ defmodule LastCrusader.Router do
   plug(:dispatch)
   # TODO: add routes!
 
-  # Simple GET Request handler for path /hello
-  get "/hello" do
-    send_resp(conn, 200, "world")
-  end
-
-  # Basic example to handle POST requests wiht a JSON body
-  post "/post" do
-    {:ok, body, conn} = read_body(conn)
-
-    # body = Poison.decode!(body)
-    # IO.inspect(body)
-
-    send_resp(conn, 201, Poison.encode!(%{created: "#{get_in(body, ["message"])}"}))
-  end
-
   # from https://dev.to/jonlunsford/elixir-building-a-small-json-endpoint-with-plug-cowboy-and-poison-1826
   # A simple route to test that the server is up
   # Note, all routes must return a connection as per the Plug spec.
@@ -42,27 +28,7 @@ defmodule LastCrusader.Router do
   end
 
   post "/events" do
-    {status, body} =
-      case conn.body_params do
-        %{"events" => events} -> {200, process_events(events)}
-        _ -> {422, missing_events()}
-      end
-
-    send_resp(conn, status, body)
-  end
-
-  defp process_events(events) when is_list(events) do
-    # Do some processing on a list of events
-    Poison.encode!(%{response: "Received Events!"})
-  end
-
-  defp process_events(_) do
-    # If we can't process anything, let them know :)
-    Poison.encode!(%{response: "Please Send Some Events!"})
-  end
-
-  defp missing_events do
-    Poison.encode!(%{error: "Expected Payload: { 'events': [...] }"})
+    Handler.handle_events(conn)
   end
 
   # "Default" route that will get called when no other route is matched
