268a151e50500f6cd4d79c1487bb78861617a428
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2020-02-23 13:53:00 +0100

    added deps: cowboy/plug/poison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule LastCrusader.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :cowboy, :plug, :poison],
       mod: {LastCrusader.Application, []}
     ]
   end
@@ -22,8 +22,10 @@ defmodule LastCrusader.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 1.5"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
