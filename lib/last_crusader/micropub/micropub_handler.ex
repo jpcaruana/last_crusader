6838fe542efd9762803a26d5b49ef7c6a8309b68
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2020-08-25 17:08:54 +0200

    micropub: reject unwanted h types

--- lib/last_crusader/micropub/micropub_handler.ex
+++ lib/last_crusader/micropub/micropub_handler.ex
@@ -6,7 +6,13 @@ defmodule LastCrusader.Micropub.MicropubHandler do
     h = conn.body_params["h"]
     content = conn.body_params["content"]
 
+    {code, content} =
+      case h do
+        "entry" -> {201, "Location: http://example.com/venue/10"}
+        _ -> {400, "bad request"}
+      end
+
     conn
-    |> send_resp(201, "Location: http://example.com/venue/10")
+    |> send_resp(code, content)
   end
 end
--- test/last_crusader/micropub_test.exs
+++ test/last_crusader/micropub_test.exs
@@ -4,7 +4,7 @@ defmodule LastCrusader.MicropubTest do
 
   @opts LastCrusader.Router.init([])
 
-  test "micropub should accept x-www-form-urlencoded requests" do
+  test "micropub should accept h-entry x-www-form-urlencoded requests" do
     conn = conn(:post, "/micropub", %{h: "entry", content: "some text content"})
 
     conn = LastCrusader.Router.call(conn, @opts)
@@ -12,5 +12,14 @@ defmodule LastCrusader.MicropubTest do
     assert conn.state == :sent
     assert conn.status == 201
     assert conn.resp_body == "Location: http://example.com/venue/10"
+  end
+
+  test "micropub should reject bad h-entries" do
+    conn = conn(:post, "/micropub", %{h: "NOPE", content: "some text content"})
+
+    conn = LastCrusader.Router.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 400
   end
 end
