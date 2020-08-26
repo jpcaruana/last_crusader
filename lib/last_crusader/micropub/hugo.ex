5015187dbe736e284cadfe855aced65066bdade9
Jean-Philippe CARUANA <jp.caruana@deepki.com>
2020-08-26 12:38:18 +0200

    hugo: easy methods for creating types

--- lib/last_crusader/micropub/hugo.ex
+++ lib/last_crusader/micropub/hugo.ex
@@ -1,6 +1,31 @@
 defmodule LastCrusader.Micropub.Hugo do
-  @moduledoc false
+  @moduledoc """
+  Generates Hugo compatile data, file content, file name
+"""
 
+  @doc """
+  Create a new Hugo Note type
+"""
+  def note(date, data) do
+    new(:note, date, data)
+  end
+
+  @doc """
+    Create a new Hugo Post type
+  """
+
+  def post(date, data) do
+    new(:post, date, data)
+  end
+
+  @doc """
+    Create a new Hugo Bookmark type
+  """
+  def bookmark(date, data) do
+    new(:bookmark, date, data)
+  end
+
+  @doc false
   def new(type, date, data) do
     %{name: name, content: content} = data
     file_name = generate_filename(type, name, date)
@@ -14,11 +39,28 @@ date = 2020-08-12T00:00:00+02:00
     {file_name, front_matter <> content}
   end
 
+  @doc """
+  Generates the complete filename (with path) for a Hugo website
+
+  Parameters:
+  - type: can be `:note` `:post` `:bookmark`
+  - name: for the file name
+  - date
+"""
   def generate_filename(:note, name, date) do
     "content/notes/" <> date <> "/" <> name <> ".md"
   end
 
-  def generate_filename(_, name, date) do
+  def generate_filename(:post, name, date) do
+    "content/posts/" <> date <> "/" <> name <> ".md"
+  end
+
+
+  def generate_filename(:bookmark, name, date) do
+    "content/bookmarks/" <> date <> "/" <> name <> ".md"
+  end
+
+  def generate_filename(_, _, _) do
     :error
   end
 end
--- test/last_crusader/hugo_test.exs
+++ test/last_crusader/hugo_test.exs
@@ -4,7 +4,7 @@ defmodule LastCrusader.HugoTest do
 
   test "it should create a Hugo file-like for Notes" do
     {file_name, file_content} =
-      Hugo.new(:note, "2020/08/12", %{name: "some-name", content: "Some markdown content\n"})
+      Hugo.note("2020/08/12", %{name: "some-name", content: "Some markdown content\n"})
 
     assert file_content == """
 +++
@@ -14,5 +14,37 @@ Some markdown content
 """
 
     assert file_name == "content/notes/2020/08/12/some-name.md"
+  end
+
+  test "it should create a Hugo file-like for Posts" do
+    {file_name, file_content} =
+      Hugo.post("2020/08/12", %{name: "some-name", content: "Some markdown content\n"})
+
+    assert file_content == """
++++
+date = 2020-08-12T00:00:00+02:00
++++
+Some markdown content
+"""
+
+    assert file_name == "content/posts/2020/08/12/some-name.md"
+  end
+
+  test "it should create a Hugo file-like for Bookmarks" do
+    {file_name, file_content} =
+      Hugo.bookmark("2020/08/12", %{name: "some-name", content: "Some markdown content\n"})
+
+    assert file_content == """
++++
+date = 2020-08-12T00:00:00+02:00
++++
+Some markdown content
+"""
+
+    assert file_name == "content/bookmarks/2020/08/12/some-name.md"
+  end
+
+  test "generate_filename should fail on inexiting type" do
+    assert :error == Hugo.generate_filename(:inexisting_type, nil, nil)
   end
 end
