5f03cdc51adc48e51670cea8c08199ab0b558f56
Jean-Philippe CARUANA <git@caruana.fr>
2020-10-22 14:48:51 +0200

    micropub: post discovery: article or note?

--- test/last_crusader/micropub/post_type_discovery_test.exs
+++ test/last_crusader/micropub/post_type_discovery_test.exs
@@ -23,7 +23,7 @@ defmodule LastCrusader.Micropub.PostTypeDiscoveryTest do
 
   test "2. in-reply-to" do
     assert :in_reply_to == discover(%{"in-reply-to": "http://some.url/"})
-    #assert :in_reply_to != discover(%{"in-reply-to": "invalid URL value"})
+    # assert :in_reply_to != discover(%{"in-reply-to": "invalid URL value"})
   end
 
   test "3. repost-of" do
@@ -42,4 +42,41 @@ defmodule LastCrusader.Micropub.PostTypeDiscoveryTest do
     assert :photo == discover(%{photo: "http://some.url/"})
   end
 
+  @doc """
+      name = get_plain_text(props.get('name'))
+      content = get_plain_text(props.get('content'))
+      if not content:
+          content = get_plain_text(props.get('summary'))
+      if content and name and is_name_a_title?(name, content):
+          return 'article'
+      return 'note'
+  """
+  test "7-15. article" do
+    assert :article == discover(%{name: "A Title", content: "Here is the content"})
+    assert :article == discover(%{name: "A Title", summary: "Here is the content"})
+    assert :article == discover(%{name: "A Title", content: "", summary: "Here is the content"})
+  end
+
+  test "always default to note post" do
+    assert :note == discover(%{description: "no content nor summary property"})
+    assert :note == discover(%{content: "no title, just plain and dull content"})
+    assert :note == discover(%{summary: "no title, just a summary"})
+    assert :note == discover(%{name: "Name is same as content", content: "Name is same as content"})
+    assert :note == discover(%{name: "Name is same as summary", summary: "Name is same as summary"})
+  end
+
+  test "find title in name: simple case" do
+    assert false == is_name_a_title?("this is the content", "this is the content")
+    assert true == is_name_a_title?("This is a title", "This is some content")
+  end
+
+  test "find title in name: common case with no explicit p-name" do
+    assert false == is_name_a_title?("nonsensethe contentnonsense", "the content")
+  end
+
+  test "find title in name: ignore case, spaces and punctuation" do
+    assert false == is_name_a_title?("the content", "ThE cONTeNT")
+    assert false == is_name_a_title?("the content", "the content...")
+    assert false == is_name_a_title?("the content", " the  content  ")
+  end
 end
