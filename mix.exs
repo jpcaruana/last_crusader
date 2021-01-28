fb15da8875470b9ce1f435378b14ac66b0ebec7b
Jean-Philippe CARUANA <git@caruana.fr>
2021-01-28 10:57:40 +0100

    release 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LastCrusader.MixProject do
   def project do
     [
       app: :last_crusader,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
