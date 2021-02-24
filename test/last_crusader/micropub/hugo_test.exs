f3c763ac7911628cec9853c3684dab2108fa2349
Jean-Philippe CARUANA <git@caruana.fr>
2021-02-24 12:15:02 +0100

    format: add describe section to HugoTest

--- test/last_crusader/micropub/hugo_test.exs
+++ test/last_crusader/micropub/hugo_test.exs
@@ -2,179 +2,181 @@ defmodule LastCrusader.HugoTest do
   use ExUnit.Case, async: true
   alias LastCrusader.Micropub.Hugo, as: Hugo
 
-  test "it should create a Hugo file-like for Notes" do
-    {file_name, file_content, web_path} =
-      Hugo.new(:note, now(), [{"content", "Some markdown content\n"}])
+  describe "Hugo.new/3" do
+    test "it should create a Hugo file-like for Notes" do
+      {file_name, file_content, web_path} =
+        Hugo.new(:note, now(), [{"content", "Some markdown content\n"}])
 
-    assert file_content == """
-           +++
-           date = "2015-01-23T23:50:07+00:00"
-           +++
-           Some markdown content
-           """
+      assert file_content == """
+             +++
+             date = "2015-01-23T23:50:07+00:00"
+             +++
+             Some markdown content
+             """
 
-    assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
-    assert web_path == "notes/2015/01/23/some-markdown-content/"
-  end
+      assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
+      assert web_path == "notes/2015/01/23/some-markdown-content/"
+    end
 
-  test "it should rename name to title" do
-    {file_name, file_content, _} =
-      Hugo.new(:note, now(), [
-        {"content", "Some other markdown content\n"},
-        {"name", "My title"}
-      ])
+    test "it should rename name to title" do
+      {file_name, file_content, _} =
+        Hugo.new(:note, now(), [
+          {"content", "Some other markdown content\n"},
+          {"name", "My title"}
+        ])
 
-    assert file_content == """
-           +++
-           date = "2015-01-23T23:50:07+00:00"
-           title = "My title"
-           +++
-           Some other markdown content
-           """
+      assert file_content == """
+             +++
+             date = "2015-01-23T23:50:07+00:00"
+             title = "My title"
+             +++
+             Some other markdown content
+             """
 
-    assert file_name == "content/notes/2015/01/23/my-title.md"
-  end
+      assert file_name == "content/notes/2015/01/23/my-title.md"
+    end
 
-  test "it should create a Hugo file-like for Notes with additional data" do
-    {file_name, file_content, _} =
-      Hugo.new(:note, now(), [
-        {"content", "Some markdown content\n"},
-        {"tags", ["tag1", "tag2"]},
-        {"copy", "https://some/url"}
-      ])
+    test "it should create a Hugo file-like for Notes with additional data" do
+      {file_name, file_content, _} =
+        Hugo.new(:note, now(), [
+          {"content", "Some markdown content\n"},
+          {"tags", ["tag1", "tag2"]},
+          {"copy", "https://some/url"}
+        ])
 
-    assert file_content == """
-           +++
-           copy = "https://some/url"
-           date = "2015-01-23T23:50:07+00:00"
-           tags = ["tag1", "tag2"]
-           +++
-           Some markdown content
-           """
+      assert file_content == """
+             +++
+             copy = "https://some/url"
+             date = "2015-01-23T23:50:07+00:00"
+             tags = ["tag1", "tag2"]
+             +++
+             Some markdown content
+             """
 
-    assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
-  end
+      assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
+    end
 
-  test "it should rename category to tags" do
-    {file_name, file_content, _} =
-      Hugo.new(:note, now(), [
-        {"content", "Some markdown content\n"},
-        {"category", ["tag1", "tag2"]}
-      ])
+    test "it should rename category to tags" do
+      {file_name, file_content, _} =
+        Hugo.new(:note, now(), [
+          {"content", "Some markdown content\n"},
+          {"category", ["tag1", "tag2"]}
+        ])
 
-    assert file_content == """
-           +++
-           date = "2015-01-23T23:50:07+00:00"
-           tags = ["tag1", "tag2"]
-           +++
-           Some markdown content
-           """
+      assert file_content == """
+             +++
+             date = "2015-01-23T23:50:07+00:00"
+             tags = ["tag1", "tag2"]
+             +++
+             Some markdown content
+             """
 
-    assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
-  end
+      assert file_name == "content/notes/2015/01/23/some-markdown-content.md"
+    end
 
-  test "it should transforms tags into a list" do
-    {_, file_content, _} =
-      Hugo.new(:note, now(), [
-        {"content", "Some markdown content\n"},
-        {"category", "one_tag"}
-      ])
+    test "it should transforms tags into a list" do
+      {_, file_content, _} =
+        Hugo.new(:note, now(), [
+          {"content", "Some markdown content\n"},
+          {"category", "one_tag"}
+        ])
 
-    assert file_content == """
-           +++
-           date = "2015-01-23T23:50:07+00:00"
-           tags = ["one_tag"]
-           +++
-           Some markdown content
-           """
-  end
+      assert file_content == """
+             +++
+             date = "2015-01-23T23:50:07+00:00"
+             tags = ["one_tag"]
+             +++
+             Some markdown content
+             """
+    end
 
-  test "it should shorten the generated slug for long content" do
-    {file_name, _, _} =
-      Hugo.new(:note, now(), [
-        {"content", "it should shorten the generated slug for long content"}
-      ])
+    test "it should shorten the generated slug for long content" do
+      {file_name, _, _} =
+        Hugo.new(:note, now(), [
+          {"content", "it should shorten the generated slug for long content"}
+        ])
 
-    assert file_name == "content/notes/2015/01/23/it-should-shorten-the-generated.md"
-  end
+      assert file_name == "content/notes/2015/01/23/it-should-shorten-the-generated.md"
+    end
 
-  test "it should shorten the generated slug for long name" do
-    {file_name, _, _} =
-      Hugo.new(:note, now(), [
-        {"content", "it should shorten the generated slug for long name"}
-      ])
+    test "it should shorten the generated slug for long name" do
+      {file_name, _, _} =
+        Hugo.new(:note, now(), [
+          {"content", "it should shorten the generated slug for long name"}
+        ])
 
-    assert file_name == "content/notes/2015/01/23/it-should-shorten-the-generated.md"
-  end
+      assert file_name == "content/notes/2015/01/23/it-should-shorten-the-generated.md"
+    end
 
-  test "it should create article" do
-    {file_name, _, _} =
-      Hugo.new(:article, now(), [
-        {"content", "some content"}
-      ])
+    test "it should create article" do
+      {file_name, _, _} =
+        Hugo.new(:article, now(), [
+          {"content", "some content"}
+        ])
 
-    assert file_name == "content/posts/2015/01/23/some-content.md"
-  end
+      assert file_name == "content/posts/2015/01/23/some-content.md"
+    end
 
-  test "it should create bookmarks" do
-    {file_name, _, _} =
-      Hugo.new(:bookmark, now(), [
-        {"content", "some content"}
-      ])
+    test "it should create bookmarks" do
+      {file_name, _, _} =
+        Hugo.new(:bookmark, now(), [
+          {"content", "some content"}
+        ])
 
-    assert file_name == "content/bookmarks/2015/01/23/some-content.md"
-  end
+      assert file_name == "content/bookmarks/2015/01/23/some-content.md"
+    end
 
-  test "it should create bookmarks with special tags: " do
-    {_, file_content, _} =
-      Hugo.new(:bookmark, now(), [
-        {"bookmark-of", "http://some-url.com/"},
-        {"category", "one_tag"}
-      ])
+    test "it should create bookmarks with special tags: " do
+      {_, file_content, _} =
+        Hugo.new(:bookmark, now(), [
+          {"bookmark-of", "http://some-url.com/"},
+          {"category", "one_tag"}
+        ])
 
-    assert file_content == """
-           +++
-           bookmark = "http://some-url.com/"
-           bookmarktags = ["one_tag"]
-           date = "2015-01-23T23:50:07+00:00"
-           +++
-           """
-  end
+      assert file_content == """
+             +++
+             bookmark = "http://some-url.com/"
+             bookmarktags = ["one_tag"]
+             date = "2015-01-23T23:50:07+00:00"
+             +++
+             """
+    end
 
-  test "it should create bookmarks with an empty content (name is random)" do
-    {file_name, _, _} =
-      Hugo.new(:bookmark, now(), [
-        {"content", nil},
-        {"bookmark-of", "http://some-url.com/"}
-      ])
+    test "it should create bookmarks with an empty content (name is random)" do
+      {file_name, _, _} =
+        Hugo.new(:bookmark, now(), [
+          {"content", nil},
+          {"bookmark-of", "http://some-url.com/"}
+        ])
 
-    assert String.contains?(file_name, "content/bookmarks/2015/01/23/")
-  end
+      assert String.contains?(file_name, "content/bookmarks/2015/01/23/")
+    end
 
-  test "it should sanitize input from emojis to prevent UnicodeConversionError from happening" do
-    {file_name, _, _} =
-      Hugo.new(:bookmark, now(), [
-        {"content",
-         "dwyl/phoenix-liveview-counter-tutorial: 🤯  beginners tutorial building a real time counter in Phoenix 1.5.5 + LiveView 0.14.7 ⚡️"},
-        {"bookmark-of", "https://github.com/dwyl/phoenix-liveview-counter-tutorial"}
-      ])
+    test "it should sanitize input from emojis to prevent UnicodeConversionError from happening" do
+      {file_name, _, _} =
+        Hugo.new(:bookmark, now(), [
+          {"content",
+           "dwyl/phoenix-liveview-counter-tutorial: 🤯  beginners tutorial building a real time counter in Phoenix 1.5.5 + LiveView 0.14.7 ⚡️"},
+          {"bookmark-of", "https://github.com/dwyl/phoenix-liveview-counter-tutorial"}
+        ])
 
-    assert file_name == "content/bookmarks/2015/01/23/dwyl-phoenix-liveview-counter.md"
-  end
+      assert file_name == "content/bookmarks/2015/01/23/dwyl-phoenix-liveview-counter.md"
+    end
 
-  test "it should sanitize input from weird '—' char to prevent strange filename from existing" do
-    {file_name, _, _} =
-      Hugo.new(:bookmark, now(), [
-        {"content", "Alembic — Monitoring Phoenix LiveView Performance"},
-        {"bookmark-of",
-         "https://alembic.com.au/blog/2021-02-05-monitoring-phoenix-liveview-performance"}
-      ])
+    test "it should sanitize input from weird '—' char to prevent strange filename from existing" do
+      {file_name, _, _} =
+        Hugo.new(:bookmark, now(), [
+          {"content", "Alembic — Monitoring Phoenix LiveView Performance"},
+          {"bookmark-of",
+           "https://alembic.com.au/blog/2021-02-05-monitoring-phoenix-liveview-performance"}
+        ])
 
-    assert file_name == "content/bookmarks/2015/01/23/alembic-monitoring-phoenix.md"
-  end
+      assert file_name == "content/bookmarks/2015/01/23/alembic-monitoring-phoenix.md"
+    end
 
-  test "generate_filename should fail on inexiting type" do
-    assert :error == Hugo.generate_path(:inexisting_type, nil, nil)
+    test "generate_filename should fail on inexiting type" do
+      assert :error == Hugo.generate_path(:inexisting_type, nil, nil)
+    end
   end
 
   def now() do
