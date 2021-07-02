f2742939e22f46a30a97f8e2f80019e0b871c513
Jean-Philippe CARUANA <git@caruana.fr>
2021-07-02 17:24:47 +0200

    github: update file

--- lib/last_crusader/micropub/github.ex
+++ lib/last_crusader/micropub/github.ex
@@ -2,6 +2,7 @@ defmodule LastCrusader.Micropub.GitHub do
   @moduledoc """
     Posts content to github
   """
+  alias Poison, as: Json
 
   @doc """
   Creates a commit with the filecontent to GitHub
@@ -23,10 +24,54 @@ defmodule LastCrusader.Micropub.GitHub do
     end
   end
 
+  @doc """
+  Updates a file on GitHub
+  """
+  @spec new_file(map(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
+          {:error, atom()} | {:ok, any()}
+  def update_file(auth, user, repo, filename, filecontent, branch \\ "master") do
+    client = build_client(auth)
+
+    with {:ok, sha} <- get_file_sha(client, user, repo, filename, branch),
+         {:ok, result} <- update_file(client, user, repo, filename, filecontent, sha, branch) do
+      {:ok, result}
+    else
+      _ -> {:ko, :github_error}
+    end
+  end
+
   # see Github API documentation:
   # https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
   defp commit_new_file(client, user, repo, filename, body) do
     Tesla.put(client, "/repos/#{user}/#{repo}/contents/#{filename}", body)
+  end
+
+  defp get_file_content(client, user, repo, filename, ref) do
+    Tesla.get(client, "/repos/#{user}/#{repo}/contents/#{filename}", query: [ref: ref])
+  end
+
+  defp update_file(client, user, repo, filename, filecontent, sha, branch) do
+    body = %{
+      "branch" => branch,
+      "message" => "update #{filename}\n\nposted with LastCrusader :)",
+      "sha" => sha,
+      "content" => Base.encode64(filecontent)
+    }
+
+    case client
+         |> commit_new_file(user, repo, filename, body) do
+      {:ok, %Tesla.Env{status: 200}} -> {:ok, :content_updated}
+      _ -> :ko
+    end
+  end
+
+  defp get_file_sha(client, user, repo, filename, ref) do
+    case client
+         |> get_file_content(user, repo, filename, ref) do
+      {:ok, %Tesla.Env{status: 200, body: %{sha: sha}}} -> {:ok, sha}
+      {:ok, %Tesla.Env{status: 200, body: body}} -> {:ok, Json.decode!(body)["sha"]}
+      _ -> :ko
+    end
   end
 
   defp build_client(auth) do
--- test/last_crusader/micropub/github_test.exs
+++ test/last_crusader/micropub/github_test.exs
@@ -2,59 +2,169 @@ defmodule LastCrusader.Micropub.GitHubTest do
   use ExUnit.Case, async: true
   import Tesla.Mock
   alias LastCrusader.Micropub.GitHub
+  alias Poison, as: Json
 
-  test "github file creation success" do
-    doc = %Tesla.Env{
-      status: 201,
-      body: ok_body(),
-      headers: [
-        {"Status", "201 Created"},
-        {"Content-Type", "application/json; charset=utf-8"},
-        {"Content-Length", "1977"},
-        {"Server", "GitHub.com"},
-        {"X-GitHub-Media-Type", "github.v3; format=json"}
-      ]
-    }
+  describe "GitHub.new_file/6" do
+    test "file creation success" do
+      doc = %Tesla.Env{
+        status: 201,
+        body: ok_create_body(),
+        headers: [
+          {"Status", "201 Created"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
 
-    mock(fn %{method: :put} -> {:ok, doc} end)
+      mock(fn %{method: :put} -> {:ok, doc} end)
 
-    assert GitHub.new_file(
-             %{access_token: "THIS_SHOULD_STAY_A_SECRET"},
-             "github_user",
-             "github_repo",
-             "test.txt",
-             "this is a text file",
-             "test"
-           ) == {:ok, :content_created}
+      assert GitHub.new_file(
+               %{access_token: "THIS_SHOULD_STAY_A_SECRET"},
+               "github_user",
+               "github_repo",
+               "test.txt",
+               "this is a text file",
+               "test"
+             ) == {:ok, :content_created}
+    end
+
+    test "file creation error" do
+      doc = %Tesla.Env{
+        status: 401,
+        body:
+          "{\"message\":\"Bad credentials\",\"documentation_url\":\"https://docs.github.com/rest\"}",
+        headers: [
+          {"Status", "401"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "80"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
+
+      mock(fn %{method: :put} -> {:ok, doc} end)
+
+      assert GitHub.new_file(
+               %{access_token: "bad credentials"},
+               "github_user",
+               "github_repo",
+               "test.txt",
+               "this is a text file",
+               "test"
+             ) == {:ko, :github_error}
+    end
   end
 
-  test "github file creation error" do
-    doc = %Tesla.Env{
-      status: 401,
-      body:
-        "{\"message\":\"Bad credentials\",\"documentation_url\":\"https://docs.github.com/rest\"}",
-      headers: [
-        {"Status", "401"},
-        {"Content-Type", "application/json; charset=utf-8"},
-        {"Content-Length", "80"},
-        {"Server", "GitHub.com"},
-        {"X-GitHub-Media-Type", "github.v3; format=json"}
-      ]
-    }
+  describe "GitHub.update_file/6" do
+    test "file update success" do
+      sha_doc = %Tesla.Env{
+        status: 200,
+        body: ok_get_sha_body(),
+        headers: [
+          {"Status", "200 OK"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
 
-    mock(fn %{method: :put} -> {:ok, doc} end)
+      updated_doc = %Tesla.Env{
+        status: 200,
+        body: ok_create_body(),
+        headers: [
+          {"Status", "200 OK"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
 
-    assert GitHub.new_file(
-             %{access_token: "bad credentials"},
-             "github_user",
-             "github_repo",
-             "test.txt",
-             "this is a text file",
-             "test"
-           ) == {:ko, :github_error}
+      mock(fn
+        %{method: :get} -> {:ok, sha_doc}
+        %{method: :put} -> {:ok, updated_doc}
+      end)
+
+      assert GitHub.update_file(
+               %{access_token: "THIS_SHOULD_STAY_A_SECRET"},
+               "github_user",
+               "github_repo",
+               "test.txt",
+               "this is a text file",
+               "test"
+             ) == {:ok, :content_updated}
+    end
+
+    test "file update failure on PUT" do
+      sha_doc = %Tesla.Env{
+        status: 200,
+        body: ok_get_sha_body(),
+        headers: [
+          {"Status", "200 OK"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
+
+      updated_doc = %Tesla.Env{
+        status: 404,
+        body: Json.encode!(%{"error" => "error", "reason" => "failed"}),
+        headers: [
+          {"Status", "404 Not Found"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
+
+      mock(fn
+        %{method: :get} -> {:ok, sha_doc}
+        %{method: :put} -> {:ok, updated_doc}
+      end)
+
+      assert GitHub.update_file(
+               %{access_token: "THIS_SHOULD_STAY_A_SECRET"},
+               "github_user",
+               "github_repo",
+               "test.txt",
+               "this is a text file",
+               "test"
+             ) == {:ko, :github_error}
+    end
+
+    test "file update failure on SHA discovery: 404" do
+      sha_doc = %Tesla.Env{
+        status: 404,
+        body: Json.encode!(%{"error" => "error", "reason" => "not found"}),
+        headers: [
+          {"Status", "404 OK"},
+          {"Content-Type", "application/json; charset=utf-8"},
+          {"Content-Length", "1977"},
+          {"Server", "GitHub.com"},
+          {"X-GitHub-Media-Type", "github.v3; format=json"}
+        ]
+      }
+
+      mock(fn %{method: :get} -> {:ok, sha_doc} end)
+
+      assert GitHub.update_file(
+               %{access_token: "THIS_SHOULD_STAY_A_SECRET"},
+               "github_user",
+               "github_repo",
+               "test.txt",
+               "this is a text file",
+               "test"
+             ) == {:ko, :github_error}
+    end
   end
 
-  defp ok_body() do
+  defp ok_create_body() do
     "{\"content\":{\"name\":\"test.txt\",\"path\":\"test.txt\",\"sha\":\"e068544d654f426eb0b145e20d8338069b4c3851\",\"size\":19,\"url\":\"https://api.github.com/repos/github_user/github_repo/co
 ntents/test.txt?ref=test\",\"html_url\":\"https://github.com/github_user/github_repo/blob/test/test.txt\",\"git_url\":\"https://api.github.com/repos/github_user/github_repo/git/blobs/e068544d654f426eb0b14
 5e20d8338069b4c3851\",\"download_url\":\"https://raw.githubusercontent.com/github_user/github_repo/test/test.txt?token=AAAHXINDV4JXZDK4NHEGWCC7XUXY6\",\"type\":\"file\",\"_links\":{\"self\":\"https://api.
@@ -66,5 +176,29 @@ f572c8bca4f1058aaa876d3352f99a12f429\",\"author\":{\"name\":\"Some Github Userna
 7a9444e13cec439d5f4a9e7404a7\"},\"message\":\"some commit message\\n\\nposted with LastCrusader :)\",\"parents\":[{\"sha\":\"3b2d9c9fd7843002b233cbfef13c8acbf6703521\",\"url\":\"https://api.github.com/rep
 os/github_user/github_repo/git/commits/3b2d9c9fd7843002b233cbfef13c8acbf6703521\",\"html_url\":\"https://github.com/github_user/github_repo/commit/3b2d9c9fd7843002b233cbfef13c8acbf6703521\"}],\"verificati
 on\":{\"verified\":false,\"reason\":\"unsigned\",\"signature\":null,\"payload\":null}}}"
+  end
+
+  defp ok_get_sha_body() do
+    Json.encode!(%{
+      "_links" => %{
+        "git" =>
+          "https://api.github.com/repos/jpcaruana/jp.caruana.fr/git/blobs/0bbca6e0fc897c9b0122f84e09c349f0e0ae98ea",
+        "html" => "https://github.com/jpcaruana/jp.caruana.fr/blob/test/test1",
+        "self" => "https://api.github.com/repos/jpcaruana/jp.caruana.fr/contents/test1?ref=test"
+      },
+      "content" => "c29tZSBjb250ZW50IGluIHRoZSBmaWxl\n",
+      "download_url" =>
+        "https://raw.githubusercontent.com/jpcaruana/jp.caruana.fr/test/test1?token=AAAHXIMSLMW5HG7S6G4OCKTA34T7A",
+      "encoding" => "base64",
+      "git_url" =>
+        "https://api.github.com/repos/jpcaruana/jp.caruana.fr/git/blobs/0bbca6e0fc897c9b0122f84e09c349f0e0ae98ea",
+      "html_url" => "https://github.com/jpcaruana/jp.caruana.fr/blob/test/test1",
+      "name" => "test1",
+      "path" => "test1",
+      "sha" => "0bbca6e0fc897c9b0122f84e09c349f0e0ae98ea",
+      "size" => 24,
+      "type" => "file",
+      "url" => "https://api.github.com/repos/jpcaruana/jp.caruana.fr/contents/test1?ref=test"
+    })
   end
 end
