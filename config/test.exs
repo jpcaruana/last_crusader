3162cdab9747fe346decc2a8480ddb41d95c43fe
Jean-Philippe CARUANA <git@caruana.fr>
2022-03-10 18:54:18 +0100

    fix false config duplication

--- config/test.exs
+++ config/test.exs
@@ -5,3 +5,28 @@ config :last_crusader,
 
 config :tesla, adapter: Tesla.Mock
 config :logger, level: :critical
+
+config :last_crusader,
+  me: "https://some.url.fr/",
+  micropub_issuer: "https://some.issuer.com/token",
+  github_user: "some_uer",
+  github_repo: "some_repo",
+  github_branch: "master",
+  github_auth: %{
+    access_token: "THIS IS A SECRET"
+  },
+  micropub_config: %{
+    types: %{
+      "post-types": [
+        %{type: "note", name: "Note"}
+      ]
+    },
+    "syndicate-to": [
+      %{
+        uid: "https://twitter.com/some_twitter_account",
+        name: "Twitter"
+      }
+    ]
+  },
+  webmention_nb_tries: 15,
+  port: 4001
