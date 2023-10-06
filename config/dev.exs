4ed9b1f2136d57526d8da791f15553d52982e8e6
Jean-Philippe CARUANA <git@caruana.fr>
2023-10-06 10:30:03 +0200

    sentry upgrade: fix config

--- config/dev.exs
+++ config/dev.exs
@@ -5,3 +5,6 @@ config :tesla, adapter: Tesla.Mock
 config :logger, :console,
   level: :critical,
   always_evaluate_messages: true
+
+config :sentry,
+  environment_name: :dev
--- config/prod.exs
+++ config/prod.exs
@@ -4,3 +4,13 @@ config :logger, :console,
   level: :info,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+
+config :sentry,
+  dsn: "https://xxx@o122392.ingest.sentry.io/12345",
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_paths: [File.cwd!()],
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
