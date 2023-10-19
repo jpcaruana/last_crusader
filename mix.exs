24e3b62122871f3888e1cfca1b9af6d1644dd8e3
Jean-Philippe CARUANA <git@caruana.fr>
2023-10-19 17:31:18 +0200

    build: add mix_audit check
    see https://github.com/mirego/mix_audit

--- mix.exs
+++ mix.exs
@@ -71,7 +71,8 @@ defmodule LastCrusader.MixProject do
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.15.0", only: [:dev], runtime: false},
-      {:git_hooks, "~> 0.7.0", only: [:test, :dev], runtime: false}
+      {:git_hooks, "~> 0.7.0", only: [:test, :dev], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
     ]
   end
 
