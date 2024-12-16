484a1e5fe11209fb30505bf9a0e9cd4afd0e62ae
Jean-Philippe CARUANA <git@caruana.fr>
2024-12-16 09:30:46 +0100

    CI: remove elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LastCrusader.MixProject do
     [
       app: :last_crusader,
       version: "0.11.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() in [:prod, :perso],
       deps: deps(),
